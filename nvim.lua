41d44dfb1f810b30a2e5c8cd5ef9673279038b4e
David Briscoe <idbrii@gmail.com>
2024-07-24 14:19:53 -0700

    nvim: Severely reduce cursor anim
    Even at 0.007 it's too much for me when it moves between windows. Maybe
    I should just turn if off...

--- nvim.lua
+++ nvim.lua
@@ -11,7 +11,7 @@ vim.cmd "command! -nargs=1 Gogo OpenBrowser <args>"
 
 -- Neovide {{{
 -- Turn down the flash slow cursor anims.
-vim.g.neovide_cursor_animation_length = 0.007
+vim.g.neovide_cursor_animation_length = 0.002
 vim.g.neovide_position_animation_length = 0.007
 vim.g.neovide_scroll_animation_length = 0.007
 
