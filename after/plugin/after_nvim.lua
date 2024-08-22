5628ef1e70c82325156fce4050580e140a24eeb3
David Briscoe <idbrii@gmail.com>
2024-08-22 11:10:07 -0700

    nvim: Use diagnostics to display ale
    nvim goes the opposite way from vim-lsp-ale. Since there's a standard
    diagnostics system, ale supports putting all of its output in there.
    
    Not sure if these should be upstreamed to ale? The existing commands
    only include ale output whereas my new commands also include lsp issues.

--- after/plugin/after_nvim.lua
+++ after/plugin/after_nvim.lua
@@ -2,3 +2,10 @@
 -- Gogo doesn't seem to work correctly in nvim, but it's really the same as OpenBrowser.
 vim.cmd "command! -nargs=1 Gogo OpenBrowser <args>"
 
+if vim.g.loaded_ale then
+    -- ale_use_neovim_diagnostics_api sends all ale lint to nvim's diagnostics
+    -- which also contains lsp, so populate with that instead.
+    vim.cmd "command! -bar ALEPopulateLocList  :lua vim.diagnostic.setloclist({open = true})"
+    vim.cmd "command! -bar ALEPopulateQuickfix :lua vim.diagnostic.setqflist({open = true})"
+end
+
