607eea32f396d89a87066c9459c5518a10c9e111
David Briscoe <idbrii@gmail.com>
2025-01-16 16:21:35 -0800

    Change dap icons, setup dap mappings
    Use Tom's characters for dapui because they don't render with gaps
    between them like the default ones.
    
    Setup mappings in dap windows with standard IDE debugging controls. Also
    try a da prefix that's available globally.

--- plugin/config_dap.lua
+++ plugin/config_dap.lua
@@ -53,21 +53,20 @@ vim.api.nvim_create_user_command("DapGuiToggle", function(...) return dapui.togg
 
 ---@diagnostic disable: missing-parameter
 ---@diagnostic disable-next-line: missing-parameter
-dapui.setup(
-    {
-        --~ controls = {
-        --~     icons = {
-        --~         disconnect = "",
-        --~         pause = "",
-        --~         play = "",
-        --~         run_last = "",
-        --~         step_back = "",
-        --~         step_into = "",
-        --~         step_out = "",
-        --~         step_over = "",
-        --~         terminate = "󰅜",
-        --~     },
-        --~ },
+dapui.setup({
+        controls = {
+            icons = {
+                disconnect = "",
+                pause = "",
+                play = "",
+                run_last = "",
+                step_back = "",
+                step_into = "",
+                step_out = "",
+                step_over = "",
+                terminate = "✕",
+            },
+        },
 
         floating = {
             mappings = {
@@ -97,3 +96,24 @@ end
 dap.listeners.before.event_exited.dapui_config = function()
     dapui.close()
 end
+
+
+local GRP = vim.api.nvim_create_augroup("david_dap", { clear = true })
+
+-- Map standard debug commands within dap windows.
+local function ConfigMappingsInBuffer()
+    vim.keymap.set("n", "<F5>",  dap.continue,  { buffer = true, desc = "Start debugging" })
+    vim.keymap.set("n", "<F10>", dap.step_over, { buffer = true, desc = "Step over" })
+    vim.keymap.set("n", "<F11>", dap.step_into, { buffer = true, desc = "Step into" })
+end
+vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = { "dapui_*" },
+        callback = ConfigMappingsInBuffer,
+    })
+vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+
+-- Try a leader-based approach.
+vim.keymap.set("n", "<Leader>dac", dap.continue,          { desc = "Start debugging" })
+vim.keymap.set("n", "<Leader>dao", dap.step_over,         { desc = "Step over" })
+vim.keymap.set("n", "<Leader>dai", dap.step_into,         { desc = "Step into" })
+vim.keymap.set("n", "<Leader>dab", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
