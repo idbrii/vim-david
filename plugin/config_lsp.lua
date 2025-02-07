c48c1d47a4ff2d0a266b270a94ad6b77e831e687
David Briscoe <idbrii@gmail.com>
2025-02-07 15:15:23 -0800

    lsp: Ignore .. in lua comments

--- plugin/config_lsp.lua
+++ plugin/config_lsp.lua
@@ -88,6 +88,7 @@ lspconfig.harper_ls.setup {
                 spell_check = false,  -- Doesn't respect my multiple vim dictionaries, so disable.
 
                 -- Disabled due to example code in comments.
+                ellipsis_length = false, -- .. is string concat
                 long_sentences = false,  -- Code looks like long sentences.
                 sentence_capitalization = false,  -- Code doesn't start with capital.
                 spaces = false,  -- Code has lots of spaces.
