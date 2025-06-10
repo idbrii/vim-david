ecbb3430be267fffa30923c34f00ef4b21bc054c
David Briscoe <idbrii@gmail.com>
2025-06-10 16:26:49 -0700

    wip: trying to make quicker always modifiable
    Annoying to have to `set ma`. I assume it's because of asyncrun.

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -5,3 +5,12 @@ vim.api.nvim_buf_create_user_command(0, "QuickerToggleFullPath",
         vim.g.quicker_force_full_path_name = not vim.g.quicker_force_full_path_name
         vim.cmd.Refresh()
     end, {})
+
+-- For some reason, quicker's editor setup fails to set modifiable.
+vim.api.nvim_create_autocmd("BufEnter", {
+        buffer = vim.fn.bufnr(),
+        desc = "Make quickfix modifiable for Quicker.",
+        callback = function()
+            vim.opt_local.modifiable = true
+        end,
+    })
